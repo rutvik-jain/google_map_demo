33f3938105d50db08ca129253bea13f1a906413a
rutvik-sr <rutvik.softrefine@gmail.com>
2022-08-30 18:02:10 +0530

    implemented current location update

--- lib/screens/googleMapScreen.dart
+++ lib/screens/googleMapScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapScreen extends StatefulWidget {
@@ -10,8 +11,17 @@ class GoogleMapScreen extends StatefulWidget {
 
 class _GoogleMapScreenState extends State<GoogleMapScreen> {
   Set<Marker> _markers = {};
+  GoogleMapController? googleMapController;
 
-  void _onMapCreated(GoogleMapController controller){
+
+  void _onTap(){
+    setState(() {
+      _markers.add(Marker(markerId: MarkerId('id-current'),
+      position: LatLng(23.3, 22.2)));
+    });
+  }
+
+  void _onMapCreated(GoogleMapController googleMapController){
     setState(() {
       _markers.add(
         Marker(
@@ -21,25 +31,77 @@ class _GoogleMapScreenState extends State<GoogleMapScreen> {
       );
     });
   }
+//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 token
 
+  @override
+  void dispose()async{
+    googleMapController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("Google Map"),
       ),
       body: GoogleMap(
           onMapCreated: _onMapCreated,
           markers: _markers,
-          initialCameraPosition: CameraPosition(target: LatLng(23.021290, 72.469429),zoom: 15)),
+          initialCameraPosition: CameraPosition(target: LatLng(23.021290, 72.469429),zoom: 15),),
       floatingActionButton: Align(
         alignment: Alignment.bottomCenter,
         child: FloatingActionButton(
           backgroundColor: Colors.blue,
-            onPressed: (){},
+            onPressed: () async {
+            Position position = await _determinePosition();
+
+            await googleMapController?.animateCamera(CameraUpdate.newCameraPosition(CameraPosition
+              (target: LatLng(position.latitude, position.longitude),zoom: 15)));
+
+            _markers.clear();
+            
+            _markers.add(Marker(markerId: MarkerId("Current Location"),
+                position: LatLng(position.latitude, position.longitude),
+                infoWindow: InfoWindow(title: "Softrefine Technology")));
+
+            setState(() {
+
+            });
+            },
         child: Icon(Icons.navigation),),
       ),
     );
+  }
+  Future<Position> _determinePosition() async {
+
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!serviceEnabled){
+      return Future.error("Location services are disabled");
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if(permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permissions are permanently denied");
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
+
   }
 }
